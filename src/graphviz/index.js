7969f7fc25a51017d368fd34dabec309b6b0991d
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-06-24 16:22:44 +0200

    temp commit

--- src/graphviz/index.js
+++ src/graphviz/index.js
@@ -2,50 +2,38 @@
 import cytoscape from "./cytoscape.esm.js"
 import dot_graph from "./dbg_graph.json" with {type: 'json'}
 import Convertor from "./convertor.js"
+import { Option }  from "./stdlib.js"
+import State from "./state.js"
+import Graph from "./graph.js"
+
 
 console.log(dot_graph);
 let graph = Convertor.convert(dot_graph);
-let var_names = Convertor.get_var_names(dot_graph);
-console.log(var_names);
 
+/* Setup html */
+
+let var_names = Convertor.get_var_names(dot_graph);
 let dom_vnames = document.getElementById('var-list');
 let make_div = name => {
   let div = document.createElement('div');
   div.className = "var-name";
   div.innerHTML = name;
-  div.addEventListener("click", () => alert(name));
+  div.addEventListener("click", () => {
+    state = State.set_focus(state, Option.some(name))
+  });
   return div;
 }
 let divs = var_names.map(make_div);
 divs.forEach(div => dom_vnames.appendChild(div));
 
-let style = [
-  {
-    selector: 'node',
-    css: {
-      shape: 'rectangle',
-      width: 'label',
-      height: 'label',
-      'text-wrap': 'wrap',
-      content: 'data(label)',
-      'text-valign': 'center',
-      'text-halign': 'center',
-      padding: 10,
-    }
-  }
-]
 
-let make_cytoscape = () => cytoscape({
-  container: document.getElementById('cy'),
-  elements: graph.elements,
-  style: style,
-  layout : { name: 'grid', rows: 1}
+let cy = Graph.make(graph.elements);
+let state = State.make (cy);
+
+/* Webpage dynamics */
+
+const input = document.querySelector("#depth-slider");
+input.addEventListener("input", ev => {
+  let depth = event.target.value;
+  state = State.set_depth(state, depth);
 })
-
-let cy = make_cytoscape(graph.elements);
-let a = cy.$('X').neighbourhood();
-console.log(a);
-
-console.log("We've executed");
-
-
---
+++ src/graphviz/state.js
@@ -0,0 +1,52 @@
+import {Option} from './stdlib.js'
+import Graph from './graph.js'
+
+let draw_graph = (state) => {
+  Graph.reset_style(state.cy);
+  let draw_all = () => {
+    let style = {
+      'visibility': 'visible'
+    };
+    console.log(state.cy.nodes);
+    state.cy.nodes().each(ele => ele.style(style));
+    state.cy.edges().each(ele => ele.style(style));
+  }
+  let draw_sub = (var_name) => {
+    let root = state.cy.$(`node[var_name = "${var_name}"]`)
+    for (let i = 0; i < state.depth; ++i) {
+      root = root.union(root.neighbourhood());
+    }
+    let style = {
+      'background-color': 'lightblue',
+      'visibility': "visible",
+    }
+    root.each(ele => ele.style(style))
+  };
+  let args = {fnone: draw_all, fsome: draw_sub}
+  Option.match(state.focus, args);
+}
+
+let make = (cy) => {
+  let focus = Option.none();
+  let depth = 0;
+  let state = {focus, depth, cy};
+  set_depth(state, depth);
+  draw_graph(state);
+  return state;
+}
+
+let set_focus = (state, focus) => {
+  state = {...state, focus};
+  draw_graph(state);
+  return state;
+}
+
+let set_depth = (state, depth) => {
+  state = {...state, depth};
+  let elt = document.querySelector('#depth-value');
+  elt.textContent = depth;
+  draw_graph(state);
+  return state;
+}
+
+export default {make, set_focus, set_depth}
