d5f461330c3c2c1c5290932012ab68b65525a668
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-08-07 12:49:14 +0200

    update graph generation

--- src/mlang/m_ir/dbg_info.ml
+++ src/mlang/m_ir/dbg_info.ml
@@ -5,11 +5,12 @@ module Info = struct
   let make var def vval = { var; def; vval }
 end
 
-module Graph = struct
-  include Graph.Persistent.Digraph.Abstract (struct
-    type t = string
-  end)
-end
+module Graph = Graph.Persistent.Digraph.Concrete (struct
+  include String
+
+  (* This feels weird, but String.hash was introduced in 5.0 *)
+  let hash = Hashtbl.hash
+end)
 
 type t = { graph : Graph.t; info : Info.t StrMap.t }
 
@@ -19,51 +20,11 @@ let to_json (fmt : Format.formatter) info : unit =
   let open Format in
   let open Info in
   let delim = ref "" in
-  let print_info var_name {var; def; vval; _} =
-    let scope = match var.scope with
-    | Tgv _ -> "tgv"
-    | Temp _ -> "temp"
-    | Ref -> "ref" in
-    let tgv_details = match var.scope with
-    | Temp _ | Ref -> ""
-    | Tgv _ ->
-        let is_input =
-          match Com.Var.cat_var_loc var with
-          | Com.CatVar.LocInput -> true
-          | _ -> false in
-        let cat = Com.Var.cat var in
-        let attrs =
-          let wrap s _ acc =
-            let wrapped = Format.asprintf {|"%s"|} s in
-            wrapped :: acc in
-          StrMap.fold wrap (Com.Var.attrs var) []
-          |> String.concat ", " |> Format.asprintf "[%s]"
-        in
-        let given_back = Com.Var.is_given_back var in
-        let descr = Pos.unmark @@ Com.Var.descr var
-          |> Re.Str.global_replace (Re.Str.regexp "	") "  " in
-        let str =
-          Format.asprintf {|, "tgv_details": {
-      "cat": "%a", "is_input": %b, "given_back": %b, "attrs": %s, "descr": "%s"
-    }|}
-          Com.CatVar.pp cat is_input given_back attrs descr in 
-        str
-    in
-    let pp_string fmt s = fprintf fmt "%s" s in
-    let pp_none fmt () = fprintf fmt "" in
-    let pp_opt = pp_print_option ~none:pp_none pp_string in
-    Format.asprintf
-      {|{ "name": "%s", "def": "%a", "value": "%a", "scope": "%s" %s}|}
-      var_name pp_opt def Com.format_literal vval scope tgv_details in
   Format.fprintf fmt "{\"graph\":[";
   let pp_vertex v =
-        let var_name = Graph.V.label v in
-    let dbg_info = StrMap.find_opt var_name info.info in
-    let obj = match dbg_info with
-    | None -> Format.asprintf {|{"name": "%s"}|} var_name
-    | Some info -> print_info var_name info in
-    Format.fprintf fmt
-      {|%s@.{"data": %s}|} !delim obj;
+    let var_name = Graph.V.label v in
+    let obj = Format.asprintf {|{"name": "%s"}|} var_name in
+    Format.fprintf fmt {|%s@.{"data": %s}|} !delim obj;
     (* Small hack to avoid trailing commas *)
     delim := ","
   in
@@ -78,7 +39,56 @@ let to_json (fmt : Format.formatter) info : unit =
   in
   Format.printf "writing edges...@.";
   Graph.iter_edges_e print_edge info.graph;
-  Format.fprintf fmt "]}@."
+  let print_info var_name { var; def; vval; _ } =
+    let scope =
+      match var.scope with Tgv _ -> "tgv" | Temp _ -> "temp" | Ref -> "ref"
+    in
+    let tgv_details =
+      match var.scope with
+      | Temp _ | Ref -> ""
+      | Tgv _ ->
+          let is_input =
+            match Com.Var.cat_var_loc var with
+            | Com.CatVar.LocInput -> true
+            | _ -> false
+          in
+          let cat = Com.Var.cat var in
+          let attrs =
+            let wrap s _ acc =
+              let wrapped = Format.asprintf {|"%s"|} s in
+              wrapped :: acc
+            in
+            StrMap.fold wrap (Com.Var.attrs var) []
+            |> String.concat ", " |> Format.asprintf "[%s]"
+          in
+          let given_back = Com.Var.is_given_back var in
+          let descr =
+            Pos.unmark @@ Com.Var.descr var
+            |> Re.Str.global_replace (Re.Str.regexp "\t") "  "
+          in
+          let str =
+            Format.asprintf
+              {|, "tgv_details": {
+      "cat": "%a", "is_input": %b, "given_back": %b, "attrs": %s, "descr": "%s"
+    }|}
+              Com.CatVar.pp cat is_input given_back attrs descr
+          in
+          str
+    in
+    let pp_string fmt s = fprintf fmt "%s" s in
+    let pp_none fmt () = fprintf fmt "" in
+    let pp_opt = pp_print_option ~none:pp_none pp_string in
+    Format.fprintf fmt
+      {|%s"%s": {"def": "%a", "value": "%a", "scope": "%s" %s}|} !delim var_name
+      pp_opt def Com.format_literal vval scope tgv_details;
+    delim := ","
+  in
+  Format.fprintf fmt "],@.";
+  Format.printf "writing info...@.";
+  delim := "";
+  Format.fprintf fmt {|"info": {@.|};
+  StrMap.iter print_info info.info;
+  Format.fprintf fmt "}}@."
 
 let write_json_file filename info =
   let filename = filename ^ ".json" in
