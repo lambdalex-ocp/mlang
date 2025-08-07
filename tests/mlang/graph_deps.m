167b946ba615e592f62c94862ece2aef3e26fa8b
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-08-07 17:15:58 +0200

    update m test file

--- tests/mlang/graph_deps.m
+++ tests/mlang/graph_deps.m
@@ -5,6 +5,10 @@ X: calculee restituee primrest = 0: "x";
 Y: calculee primrest = 0: "y";
 TXMARJ: calculee primrest = 0: "tx_marj";
 ANNEE: calculee primrest = 0: "annee";
+BLABLA: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias V_BLA: "blabla";
+INPUT_UNDEFINED: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias IUND: "blabla";
+INPUT_DEFINED: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias IDEF: "blabla";
+
 
 regle 1337:
 application: app;
@@ -12,7 +16,7 @@ application: app;
 X = 3;
 Y = 3 + X;
 TXMARJ = Y;
-ANNEE = TXMARJ;
+ANNEE = TXMARJ + INPUT_UNDEFINED + INPUT_DEFINED;
 cible target:
 application: app;
 calculer domaine primitive;
