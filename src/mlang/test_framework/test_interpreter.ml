524737557405e7255532740ea04b3887f384824f
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-08-12 16:28:23 +0200

    test_interpreter remove debug comments

--- src/mlang/test_framework/test_interpreter.ml
+++ src/mlang/test_framework/test_interpreter.ml
@@ -198,11 +198,9 @@ let check_test ?(files : string list option) (program : Mir.program)
                (not set == origin = Declared) *)
             let add_to_map var lit map =
               let name = Com.Var.name_str var in
-              Format.printf "@.adding %s" name;
               StrMap.update name
                 (function
                   | Some Dbg_info.Info.{ origin = Declared; _ } | None ->
-                      Format.printf " - no info";
                       let def = Some "input-set" in
                       let rule_id = Dbg_info.Origin.Input in
                       let info = Dbg_info.Info.make var def lit rule_id in
