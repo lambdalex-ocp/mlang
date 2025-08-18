a642e2bdff1a3583e734b48ba41d450cfd081ee0
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-08-18 16:26:22 +0200

    update graph_deps.m

--- tests/mlang/graph_deps.m
+++ tests/mlang/graph_deps.m
@@ -3,6 +3,7 @@ application app;
 V_ANCSDED : saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias V_POUET: "v_ancsed";
 X: calculee restituee primrest = 0: "x";
 Y: calculee primrest = 0: "y";
+MULTILINE: calculee primrest = 0 : "multiline";
 TXMARJ: calculee primrest = 0: "tx_marj";
 ANNEE: calculee primrest = 0: "annee";
 BLABLA: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias V_BLA: "blabla";
@@ -17,6 +18,8 @@ X = 3;
 Y = 3 + X;
 TXMARJ = Y;
 ANNEE = TXMARJ + INPUT_UNDEFINED + INPUT_DEFINED;
+MULTILINE = X
++ Y;
 cible target:
 application: app;
 calculer domaine primitive;
