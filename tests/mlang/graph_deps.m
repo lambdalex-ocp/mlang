8d1ce251ba111ea4dcec269c07abbedc8b262c02
Alexandre Doussot <alexandre.doussot@ocamlpro.com>
2025-09-08 17:30:34 +0200

    update graph_deps to use consts

--- tests/mlang/graph_deps.m
+++ tests/mlang/graph_deps.m
@@ -6,6 +6,7 @@ Y: calculee primrest = 0: "y";
 MULTILINE: calculee primrest = 0 : "multiline";
 TXMARJ: calculee primrest = 0: "tx_marj";
 ANNEE: calculee primrest = 0: "annee";
+CONST: const=6;
 BLABLA: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias V_BLA: "blabla";
 INPUT_UNDEFINED: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias IUND: "blabla";
 INPUT_DEFINED: saisie revenu acompte = 0 avfisc = 0 categorie_TL = 0 classe = 0 cotsoc = 0 ind_abat = 0 modcat = 0 nat_code = 0 primrest = 0 priorite = 0 rapcat = 0 sanction = 0 alias IDEF: "blabla";
@@ -15,8 +16,8 @@ regle 1337:
 application: app;
 
 X = 3;
-Y = 3 + X;
-TXMARJ = Y;
+Y = 3 + X * CONST;
+TXMARJ = Y - CONST;
 ANNEE = TXMARJ + INPUT_UNDEFINED + INPUT_DEFINED;
 MULTILINE = X
 + Y;
